e263bae050e812d3ee1bd37057c4ca415ff36849
ankit <ankit@ankit-desktop>
2018-02-09 21:47:30 +0530

    Initial Commit

---
+++ src/test/java/com/umbrella/FlipKartLive/FunctionalTest/VerifyYouTubeFunctionality.java
@@ -0,0 +1,29 @@
+package com.umbrella.FlipKartLive.FunctionalTest;
+
+
+
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
+
+import com.umbrella.FlipKartLive.generics.LoggerHelper;
+import com.umbrella.FlipKartLive.testCore.Config;
+import com.umbrella.FlipKartLive.testCore.TestBase;
+
+public class VerifyYouTubeFunctionality extends TestBase {
+
+	Config config;
+
+	private static final Logger log = LoggerHelper.getLogger(VerifyYouTubeFunctionality.class);
+
+
+	@Test
+	public void SkipAddTest() throws InterruptedException
+	{
+		log.info(VerifyYouTubeFunctionality.class.getName() + " Method verifyYouTube " + " Get Started");
+		config = new Config(OR);
+		driver.get(config.getWebsite());
+		log.info("++++++++++++++++++++++Just Checking Project Config++++++++++++++++++++++++++++++++++++++");
+				
+	}
+
+}
